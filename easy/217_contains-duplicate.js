539352c3d1f43fa6ce6e5d5ab61199a277a4c5e5
kylemucha <kylemuchaswe@gmail.com>
2025-04-04 23:31:56 -0700

    217 - Contains Duplicate (Attempt 2)
    After wokring on this problem before with my mentor, I was able to learn about sets and maps. I wanted to try solving this problem again to see how fast I can implement a solution and was able to solve it under 3 minutes. Really glad I am getting more comfortable with easy problems, this problem would have stumped me for awhile when I first started to leetcode.

--- easy/217_contains-duplicate.js
+++ easy/217_contains-duplicate.js
@@ -34,18 +34,18 @@
 // -109 <= nums[i] <= 109
 
 /**
-* @param {number[]} nums
+ * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
     let set = new Set();
+    let n = nums.length;
 
-    for(let i = 0; i < nums.length; i++){
-        if(set.has(nums[i])){
-            return true;
-            break;
+    for (let i = 0; i <= n; i++){
+        if(!set.has(nums[i])){
+            set.add(nums[i]);
         } else {
-            set.add(nums[i])
+            return true;
         }
     }
     return false;
